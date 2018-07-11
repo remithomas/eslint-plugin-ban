e741960e6b4eb2d4239fb3563794d5e398146858
Remi THOMAS <remi.thomas@yahoo.fr>
2018-07-10 22:02:52 -0400

    add travis + linter

--- tests/lib/rules/ban.js
+++ tests/lib/rules/ban.js
@@ -2,15 +2,15 @@
  * @fileoverview Ban methods
  * @author Rémi THOMAS
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/ban"),
+var rule = require('../../../lib/rules/ban'),
 
-    RuleTester = require("eslint").RuleTester;
+    RuleTester = require('eslint').RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -18,93 +18,93 @@ var rule = require("../../../lib/rules/ban"),
 //------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
-ruleTester.run("ban with default options", rule, {
+ruleTester.run('ban with default options', rule, {
     valid: [
         {
-            code: "// comment"
+            code: '// comment'
         },
         {
-            code: "for(item in list) {}"
+            code: 'for(item in list) {}'
         }
     ],
 
     invalid: []
 });
 
-ruleTester.run("ban with configuration for function (ban function `functionName`)", rule, {
+ruleTester.run('ban with configuration for function (ban function `functionName`)', rule, {
     valid: [
-        "foo()",
+        'foo()',
         {
-            code: "// comment functionName()",
-            options: [{"name": "functionName", "message": "Prefer use functionName2"}]
+            code: '// comment functionName()',
+            options: [{'name': 'functionName', 'message': 'Prefer use functionName2'}]
         },
         {
-            code: "for(item in list) {}",
-            options: [{"name": "functionName", "message": "Prefer use functionName2"}]
+            code: 'for(item in list) {}',
+            options: [{'name': 'functionName', 'message': 'Prefer use functionName2'}]
         },
         {
-            code: "var functionName = 'hello';",
-            options: [{"name": "functionName", "message": "Prefer use functionName2"}]
+            code: 'var functionName = \'hello\';',
+            options: [{'name': 'functionName', 'message': 'Prefer use functionName2'}]
         }
     ],
 
     invalid: [
         // Ban function
         {
-            code: "functionName('fn');",
+            code: 'functionName(\'fn\');',
             errors: [{
-                message: "Prefer use functionName2"
+                message: 'Prefer use functionName2'
             }],
-            options: [{"name": "functionName", "message": "Prefer use functionName2"}]
+            options: [{'name': 'functionName', 'message': 'Prefer use functionName2'}]
         },
         {
             code: `
                 var result = functionName('myString');
             `,
             errors: [{
-                message: "Prefer use functionName2"
+                message: 'Prefer use functionName2'
             }],
-            options: [{"name": "functionName", "message": "Prefer use functionName2"}]
+            options: [{'name': 'functionName', 'message': 'Prefer use functionName2'}]
         },
     ]
 });
 
-ruleTester.run("ban with configuration for method (ban method `push`)", rule, {
+ruleTester.run('ban with configuration for method (ban method `push`)', rule, {
     valid: [
-        "value.bar()",
+        'value.bar()',
         {
-            code: "// comment",
-            options: [{"name": ["*", "push"], "message": "Prefer use es6 spread"}]
+            code: '// comment',
+            options: [{'name': ['*', 'push'], 'message': 'Prefer use es6 spread'}]
         },
         {
-            code: "for(item in list) {}",
-            options: [{"name": ["*", "push"], "message": "Prefer use es6 spread"}]
+            code: 'for(item in list) {}',
+            options: [{'name': ['*', 'push'], 'message': 'Prefer use es6 spread'}]
         },
         {
-            code: "var functionName = 'functionName';",
-            options: [{"name": ["*", "push"], "message": "Prefer use es6 spread"}]
+            code: 'var functionName = \'functionName\';',
+            options: [{'name': ['*', 'push'], 'message': 'Prefer use es6 spread'}]
         },
         {
-            code: "animals.push('cows')",
-            options: [{"name": ["humans", "push"], "message": "Prefer use es6 spread"}]
+            code: 'animals.push(\'cows\')',
+            options: [{'name': ['humans', 'push'], 'message': 'Prefer use es6 spread'}]
         }
     ],
 
     invalid: [
         // Ban method
         {
-            code: "animals.push('dogs')",
+            code: 'animals.push(\'dogs\')',
             errors: [{
-                message: "Prefer use es6 spread"
+                message: 'Prefer use es6 spread'
             }],
-            options: [{"name": ["*", "push"], "message": "Prefer use es6 spread"}]
+            options: [{'name': ['*', 'push'], 'message': 'Prefer use es6 spread'}]
         },
         {
-            code: "animals.push('cows')",
+            code: 'animals.push(\'cows\')',
             errors: [{
-                message: "Prefer use es6 spread"
+                message: 'Prefer use es6 spread'
             }],
-            options: [{"name": ["animals", "push"], "message": "Prefer use es6 spread"}]
+            options: [{'name': ['animals', 'push'], 'message': 'Prefer use es6 spread'}]
         }
     ]
 });
